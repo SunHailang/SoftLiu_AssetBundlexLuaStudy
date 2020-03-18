33ee283d5a1879c843e5e4fc7411e45f943dfc8e
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-03-18 18:52:02 +0800

    update

---
+++ Assets/Code/AssetBundles/Bundles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoftLiu.AssetBundles
+{
+
+    public enum BundleType
+    {
+        LevelsType,
+        PrefabType,
+        ScriptsType,
+    }
+
+    public class Bundles : ScriptableObject
+    {
+        [SerializeField]
+        private IBundle[] m_bundles;
+
+    }
+}
