67e8be5e84573fb948cf9d9c24434292b35531fd
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-03-18 18:52:02 +0800

    update

---
+++ Assets/Code/AssetBundles/Data/LevesBundle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoftLiu.AssetBundles
+{
+    [System.Serializable]
+    public class LevesBundle : IBundle
+    {
+        [SerializeField]
+        public string bundlePath { get; set; }
+
+    }
+}
---
+++ Assets/Code/AssetBundles/Interfaces/IBundle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoftLiu.AssetBundles
+{
+    public interface IBundle
+    {
+        string bundlePath { get; set; }
+
+    }
+}
