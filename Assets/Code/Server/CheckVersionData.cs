25957f1207be1f280d6d2772f11a8861a4794796
SunHailang <hai-lang.sun@ubisoft.com>
2020-03-22 23:32:42 +0800

    Add zip and unzip

---
+++ Assets/Code/Server/CheckVersionData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum VersionCheckType
+{
+    None,
+    UpdateType,
+    LatestType,
+}
+
+public class CheckVersionData
+{
+    public VersionCheckType m_type;
+    public string m_version = string.Empty;
+
+    public override string ToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
