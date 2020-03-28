3e58574773d219469a581d547368be5100fa28ac
SunHailang <hai-lang.sun@ubisoft.com>
2020-03-28 16:07:07 +0800

    add grid create

---
+++ Assets/Code/GUI/Scenes/Astar/AstarTesting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AstarTesting : MonoBehaviour
+{
+    public Texture2D m_texture = null;
+    [SerializeField]
+    private SpriteRenderer m_spriteRander = null;
+    [SerializeField]
+    private Transform m_parent = null;
+
+    SoftLiu_Grid grid;
+
+    private void Start()
+    {
+        grid = new SoftLiu_Grid(10, 10, 10f);
+    }
+
+
+    public void CreateGrid_OnClick()
+    {
+        grid.SetSpriteRander(m_spriteRander, m_parent, m_texture);
+    }
+
+
+}
+
