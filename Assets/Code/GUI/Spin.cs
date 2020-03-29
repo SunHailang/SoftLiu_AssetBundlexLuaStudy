36896af78bda3e1ccdbc419144a95f87837da1ea
SunHailang <hai-lang.sun@ubisoft.com>
2020-03-29 23:12:01 +0800

    PSB add Animation

---
+++ Assets/Code/GUI/Spin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_alxe = Vector3.forward;
+
+
+    private void Update()
+    {
+        //transform.Rotate(m_alxe);
+    }
+}
