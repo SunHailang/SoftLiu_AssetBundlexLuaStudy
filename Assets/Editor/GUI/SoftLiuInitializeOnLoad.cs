d2d53c5e4dd14e9a5b74d473ab3411372352be1c
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-04-01 19:03:02 +0800

    add Editor Script

---
+++ Assets/Editor/GUI/SoftLiuInitializeOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class SoftLiuInitializeOnLoad 
+{
+    static SoftLiuInitializeOnLoad()
+    {
+        EditorApplication.update += OnUpdate;
+    }
+
+    private static void OnUpdate()
+    {
+
+    }
+}
