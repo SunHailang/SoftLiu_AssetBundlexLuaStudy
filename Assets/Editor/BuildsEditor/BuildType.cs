f91c1fc773665dc1d027700b8cff24226f00591d
SunHailang <hai-lang.sun@ubisoft.com>
2020-04-06 19:22:52 +0800

    update android build

---
+++ Assets/Editor/BuildsEditor/BuildType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoftLiu.Build
+{
+
+    // <summary>
+    /// Possible build types
+    /// </summary>
+    public enum BuildType
+    {
+        Development,
+        Preproduction,
+        Production,
+        Marketing
+    }
+}
