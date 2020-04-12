8d996e08823be80db4eac3a208a443dce4e58735
SunHailang <hai-lang.sun@ubisoft.com>
2020-04-12 22:03:52 +0800

    update build android

---
+++ Assets/Code/GUI/Common/SoftLiuNativeReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftLiuNativeReceiver : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void MessageBoxClick(string msg)
+    {
+        Debug.Log("MessageBoxClick: " + msg);
+    }
+
+    public void PermissionReceivedSuccess(string msg)
+    {
+        Debug.Log("PermissionReceivedSuccess: " + msg);
+    }
+}
