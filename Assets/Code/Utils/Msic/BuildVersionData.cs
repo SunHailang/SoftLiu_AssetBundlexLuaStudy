3ff34712a900b6e270b3c9ed1a22773852872730
SunHailang <hai-lang.sun@ubisoft.com>
2020-04-15 23:31:55 +0800

    updata build gradle

--- Assets/Code/Utils/Msic/BuildVersionData.cs
+++ Assets/Code/Utils/Msic/BuildVersionData.cs
@@ -6,8 +6,13 @@ namespace SoftLiu.Misc
 {
     public class BuildVersionData
     {
+        // 用于生成JSON的字段
         public string defVersionName;
         public int defVersionCode;
         public int defTargetSdkVersion;
+        // 用于获取属性的名字
+        public string m_defVersionName { get { return defVersionName; } }
+        public int m_defVersionCode { get { return defVersionCode; } }
+        public int m_defTargetSdkVersion { get { return defTargetSdkVersion; } }
     }
 }
