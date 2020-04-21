975c9ba355503a5679cf74a04bc2d44a2cf940dc
SunHailang <hai-lang.sun@ubisoft.com>
2020-04-22 00:01:53 +0800

    update scenes

---
+++ Assets/Code/GUI/Scenes/FrontEnd/FrontEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontEnd : MonoBehaviour
+{
+
+    public void BtnSetting_OnClick()
+    {
+        Debug.Log("FrontEnd Setting Button OnClick.");
+    }
+
+    public void BtnPlay_OnClick()
+    {
+        Debug.Log("FrontEnd Play Button OnClick.");
+    }
+}
---
+++ Assets/Code/States/ScreenStates/FrontEndState.cs
@@ -0,0 +1,27 @@
+﻿using SoftLiu.State;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontEndState : ScreenState
+{
+    public override void OnEnter(State previousState, params object[] args)
+    {
+        base.OnEnter(previousState, args);
+    }
+
+    public override void OnExit(State nextState, ExitBehaviour exitBehaviour)
+    {
+        base.OnExit(nextState, exitBehaviour);
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnHardwareBackButton()
+    {
+        base.OnHardwareBackButton();
+    }
+}
