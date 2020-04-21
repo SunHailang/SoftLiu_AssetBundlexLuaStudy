038fcf61903b0cb6fd6b5950b72b080f06f8e480
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-04-21 19:09:11 +0800

    add screen controller

---
+++ Assets/Code/States/StateMachine.cs
@@ -0,0 +1,40 @@
+﻿using SoftLiu.State;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private string m_lastStateName = "";
+
+    private bool m_isStateTransitioning = false;
+    public bool isStateTransitioning
+    {
+        get { return m_isStateTransitioning; }
+        set { m_isStateTransitioning = value; }
+    }
+
+    #region States
+
+    protected Dictionary<string, State> m_states = new Dictionary<string, State>();
+
+    // State stack
+    protected Stack<State> m_stack = new Stack<State>();
+
+    public State GetStateByName(string name)
+    {
+        State state = null;
+        if (!m_states.TryGetValue(name, out state))
+            return null;
+        return state;
+    }
+
+    public bool IsState<T>() where T : State
+    {
+        return m_stack.Peek() is T;
+    }
+
+
+
+    #endregion
+}
