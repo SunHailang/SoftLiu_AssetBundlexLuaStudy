f691988df39aadbc1869c16252b708da4e7c8c10
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-04-29 18:32:43 +0800

    add Mission System

---
+++ Assets/Editor/GUI/SoftLiuMenuItemEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SoftLiuMenuItemEditor
+{
+    [MenuItem("SoftLiu/SwitchPlatform/Android")]
+    public static void SoftLiuSwitchPlatform_Android()
+    {
+        if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android)
+        {
+            Debug.Log("SwitchPlatform Error: Current Platform is Android.");
+            return;
+        }
+        SwitchActivePlatform(BuildTargetGroup.Android, BuildTarget.Android);
+        //EditorUserBuildSettings.SwitchActiveBuildTargetAsync(BuildTargetGroup.Android, BuildTarget.Android);
+    }
+
+    [MenuItem("SoftLiu/SwitchPlatform/iOS")]
+    public static void SoftLiuSwitchPlatform_iOS()
+    {
+        if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
+        {
+            Debug.Log("SwitchPlatform Error: Current Platform is iOS.");
+            return;
+        }
+        SwitchActivePlatform(BuildTargetGroup.iOS, BuildTarget.iOS);
+        //EditorUserBuildSettings.SwitchActiveBuildTargetAsync(BuildTargetGroup.Android, BuildTarget.Android);
+    }
+
+    public static bool SwitchActivePlatform(BuildTargetGroup targetGroup, BuildTarget target)
+    {
+        return EditorUserBuildSettings.SwitchActiveBuildTarget(targetGroup, target);
+    }
+}
